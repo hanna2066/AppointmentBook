af0c5ba1d1cc747db9e1e5a1b50dd05082e88519
hanna2066 <HAnnah6@nycstudents.net>
2024-11-18 16:48:42 +0000

    Setup

--- AppointmentBook.java
+++ AppointmentBook.java
@@ -1,1 +1,30 @@
+public class AppointmentBook {
+    private boolean [][]schedule;
+    public AppointmentBook(boolean[][]schedule)
+    {
+        this.schedule=schedule;
+    }
+    private boolean isMinuteFree(int period, int minute) {
+        return schedule[period-1][minute];
+        }
 
+    private void reserveBlock(int period, int startMinute, int duration) {
+         }
+
+   
+    public int findFreeBlock(int period, int duration) {
+        return 0;
+          }
+
+    public boolean makeAppointment(int startPeriod, int endPeriod, int duration){
+        return false;
+    }
+    public void printPeriod(int period)
+    {
+        for(int i = 0; i< schedule[period-1].length; i++)
+        System.out.println(i+""+schedule[period-1][i]);
+    }
+
+    // There may be instance variables, constructors, and methods that are not shown.
+
+}
--- Main.java
+++ Main.java
@@ -1,1 +1,7 @@
+public Class main{
 
+boolean [][]schedle=new boolean[8][60];
+AppointmentBook a = new AppointmentBook(schedule);
+for(int i= 10; i<15;i++) schedule [1][i]=true;
+a.printPeriod(2);
+}
