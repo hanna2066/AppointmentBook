e261a14300c325cac2dc000f4c5cd93711f08096
hanna2066 <HAnnah6@nycstudents.net>
2025-01-03 16:49:10 +0000

    edits

--- AppointmentBook.java
+++ AppointmentBook.java
@@ -1,5 +1,5 @@
 public class AppointmentBook {
-    private boolean [][]schedule;
+    private boolean [][] schedule;
     public AppointmentBook(boolean[][]schedule)
     {
         this.schedule=schedule;
@@ -13,11 +13,30 @@ public class AppointmentBook {
 
    
     public int findFreeBlock(int period, int duration) {
-        return 0;
-          }
+        int block= 0; 
+        for(int i=0; i<60; i++){
+            if(isMinuteFree(period, i)){
+                block++;
+                if(block==duration){
+                    return i-duration+1;
+                }
+            } else{
+                block =0;
+            } 
+        }
+        return -1;
+        }
+         
+
+    
 
     public boolean makeAppointment(int startPeriod, int endPeriod, int duration){
-        return false;
+       for(int i=25; i<30; i++) schedule [1][i] = true;
+       for (int i=0; i<15; i++) schedule [2][i]=true;
+       for(int i=41; i<60; i++) schedule [2][i]= true;
+       for (int i=5; i<30; i++) schedule [3][i]=true;
+       for(int i=44; i<60; i++) schedule [3][i]=true;
+       return false;
     }
     public void printPeriod(int period)
     {
